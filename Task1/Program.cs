6cf19d5d5771d8524adea7aa046a27180bb7de9e
NirjalaGhimire <nirjalaghimire012@gmail.com>
2025-12-04 08:22:35 +0545

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle { Length = 10, Breadth = 5 };
+
+            Console.WriteLine($"Area: {r.Area}");
+            Console.WriteLine($"Perimeter: {r.Perimeter()}");
+        }
+    }
+}
---
+++ Task1/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Rectangle
+    {
+        public double Length { get; set; }
+        public double Breadth { get; set; }
+
+        // Expression-bodied properties
+        public double Area => Length * Breadth;
+
+        // Expression-bodied method for perimeter
+        public double Perimeter() => 2 * (Length + Breadth);
+    }
+}
